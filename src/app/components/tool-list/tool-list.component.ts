ee630be3ef3a3861b89c754ac0c003fbf956efe6
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:56:27 +0800

    #15 tool-list

---
+++ src/app/components/tool-list/tool-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tool-list.component.html',
+  styleUrl: './tool-list.component.css'
+})
+export class ToolListComponent {
+
+}
