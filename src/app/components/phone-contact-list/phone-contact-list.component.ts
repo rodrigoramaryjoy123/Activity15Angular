f0611981b81ced3b8cf8243d40bef98d0f83429e
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:07:12 +0800

    #19 phone-contact-list

---
+++ src/app/components/phone-contact-list/phone-contact-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-contact-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-contact-list.component.html',
+  styleUrl: './phone-contact-list.component.css'
+})
+export class PhoneContactListComponent {
+
+}
