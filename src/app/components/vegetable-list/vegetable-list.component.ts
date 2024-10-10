0a0231b367e1efa77e22797ef9f1fa3e4a0d78e2
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:54:20 +0800

    #13 vegetable-list

---
+++ src/app/components/vegetable-list/vegetable-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vegetable-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './vegetable-list.component.html',
+  styleUrl: './vegetable-list.component.css'
+})
+export class VegetableListComponent {
+
+}
