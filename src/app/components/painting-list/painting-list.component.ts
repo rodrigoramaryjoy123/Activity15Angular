bd58b3eb39576fa17b5ad2a58bd2c01e90b92b23
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:23:03 +0800

    #38 painting-list

---
+++ src/app/components/painting-list/painting-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painting-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './painting-list.component.html',
+  styleUrl: './painting-list.component.css'
+})
+export class PaintingListComponent {
+
+}
