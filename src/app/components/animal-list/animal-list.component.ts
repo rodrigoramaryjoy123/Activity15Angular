cf7d1fb20fd14784208abc6cc30604127be671c3
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:55:37 +0800

    #14 animal-list

---
+++ src/app/components/animal-list/animal-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-list.component.html',
+  styleUrl: './animal-list.component.css'
+})
+export class AnimalListComponent {
+
+}
