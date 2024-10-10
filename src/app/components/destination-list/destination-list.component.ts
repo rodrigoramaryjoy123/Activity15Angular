4d8bc0b179b35df5eb205e626839f716b25c9204
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:15:54 +0800

    #26 stationery-list

---
+++ src/app/components/destination-list/destination-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './destination-list.component.html',
+  styleUrl: './destination-list.component.css'
+})
+export class DestinationListComponent {
+
+}
---
+++ src/app/components/stationery-list/stationery-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stationery-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './stationery-list.component.html',
+  styleUrl: './stationery-list.component.css'
+})
+export class StationeryListComponent {
+
+}
