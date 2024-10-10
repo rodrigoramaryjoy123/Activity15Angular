2302fef6d7e125549cb348c50c22319bd18b9e23
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:22:06 +0800

    #36 accessory-list

---
+++ src/app/components/accessory-list/accessory-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accessory-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './accessory-list.component.html',
+  styleUrl: './accessory-list.component.css'
+})
+export class AccessoryListComponent {
+
+}
