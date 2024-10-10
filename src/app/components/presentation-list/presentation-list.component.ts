8502e9438cf59924b78bfedf05b95eb3220e6508
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:25:55 +0800

    #45 presentation-list

---
+++ src/app/components/presentation-list/presentation-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentation-list.component.html',
+  styleUrl: './presentation-list.component.css'
+})
+export class PresentationListComponent {
+
+}
