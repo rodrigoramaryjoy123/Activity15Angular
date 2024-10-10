a38501bb5263a3ff095fefefc1a77e3fdc8dcb54
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:18:33 +0800

    #29 laptop-list

---
+++ src/app/components/laptop-list/laptop-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laptop-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './laptop-list.component.html',
+  styleUrl: './laptop-list.component.css'
+})
+export class LaptopListComponent {
+
+}
