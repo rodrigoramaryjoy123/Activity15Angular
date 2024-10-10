22cbf654980a1695af5e527fd5f02abaf1a9e40a
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:04:27 +0800

    #18 software-list

---
+++ src/app/components/software-list/software-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-software-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './software-list.component.html',
+  styleUrl: './software-list.component.css'
+})
+export class SoftwareListComponent {
+
+}
