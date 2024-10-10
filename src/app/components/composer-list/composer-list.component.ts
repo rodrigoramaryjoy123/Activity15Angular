7a369da54511bee4e970d02f641206a5f85a7249
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:24:07 +0800

    #40 composer-list

---
+++ src/app/components/composer-list/composer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-composer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './composer-list.component.html',
+  styleUrl: './composer-list.component.css'
+})
+export class ComposerListComponent {
+
+}
