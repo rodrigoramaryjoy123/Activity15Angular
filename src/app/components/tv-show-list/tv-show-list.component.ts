8e0b6e17623d76cb80d9c8d975baa0dbd5f0a4aa
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:21:41 +0800

    #34 tv-show-list

---
+++ src/app/components/tv-show-list/tv-show-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-show-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tv-show-list.component.html',
+  styleUrl: './tv-show-list.component.css'
+})
+export class TvShowListComponent {
+
+}
