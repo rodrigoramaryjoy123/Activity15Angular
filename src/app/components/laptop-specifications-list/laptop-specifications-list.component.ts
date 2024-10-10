3783eafd325340ee8237cf3b29e021abfb0770d8
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:20:27 +0800

    #30 laptop-specifications-list

---
+++ src/app/components/laptop-specifications-list/laptop-specifications-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laptop-specifications-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './laptop-specifications-list.component.html',
+  styleUrl: './laptop-specifications-list.component.css'
+})
+export class LaptopSpecificationsListComponent {
+
+}
