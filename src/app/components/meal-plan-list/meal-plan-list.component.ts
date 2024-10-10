3f731ee540c03e9edaa8bc35918afdd996f82b66
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:25:14 +0800

    #43 meal-plan-list

---
+++ src/app/components/meal-plan-list/meal-plan-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-plan-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './meal-plan-list.component.html',
+  styleUrl: './meal-plan-list.component.css'
+})
+export class MealPlanListComponent {
+
+}
