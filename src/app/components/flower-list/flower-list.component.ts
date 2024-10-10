32791f3b1f4d4868d6e57939e2465bea961ca603
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:16:36 +0800

    #27 flower-list

---
+++ src/app/components/flower-list/flower-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flower-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './flower-list.component.html',
+  styleUrl: './flower-list.component.css'
+})
+export class FlowerListComponent {
+
+}
