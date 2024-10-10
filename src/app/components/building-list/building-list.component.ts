13c275d9c0c1edc3c00fffa8c5f178ed94b72757
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:22:36 +0800

    #37 building-list

---
+++ src/app/components/building-list/building-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-building-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './building-list.component.html',
+  styleUrl: './building-list.component.css'
+})
+export class BuildingListComponent {
+
+}
