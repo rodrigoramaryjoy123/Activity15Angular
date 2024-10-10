b2f9d25fa47839b7e8ad567241c99a47f147bcda
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:40:57 +0800

    #3 fruit-list

---
+++ src/app/components/fruit-list/fruit-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruit-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './fruit-list.component.html',
+  styleUrl: './fruit-list.component.css'
+})
+export class FruitListComponent {
+
+}
