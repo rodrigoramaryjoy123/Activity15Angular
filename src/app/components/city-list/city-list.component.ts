e644c18a0a84e3c31d07e81addbb4db4aea2be84
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:44:49 +0800

    #6 city-list

---
+++ src/app/components/city-list/city-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './city-list.component.html',
+  styleUrl: './city-list.component.css'
+})
+export class CityListComponent {
+
+}
