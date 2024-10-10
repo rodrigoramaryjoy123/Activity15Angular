a6af9333841ec63dea6a102f5520349448440ed0
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:09:24 +0800

    #20 music-playlist

---
+++ src/app/components/music-playlist/music-playlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-playlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './music-playlist.component.html',
+  styleUrl: './music-playlist.component.css'
+})
+export class MusicPlaylistComponent {
+
+}
