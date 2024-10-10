98157593de9ac10368d5225daf3eea3712884e67
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:14:52 +0800

    #25 lecture-list

---
+++ src/app/components/lecture-list/lecture-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecture-list.component.html',
+  styleUrl: './lecture-list.component.css'
+})
+export class LectureListComponent {
+
+}
