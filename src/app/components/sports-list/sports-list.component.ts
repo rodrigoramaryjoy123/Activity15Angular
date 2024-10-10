daac47fe449274b41c9d4bcb5c2a8c6008f402ce
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:53:57 +0800

    #12 sports-list

---
+++ src/app/components/sports-list/sports-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sports-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './sports-list.component.html',
+  styleUrl: './sports-list.component.css'
+})
+export class SportsListComponent {
+
+}
