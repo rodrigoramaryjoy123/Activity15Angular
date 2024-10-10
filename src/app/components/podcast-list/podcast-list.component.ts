00d0e3333f9ed0ce65b2c727a071abecd83bbe57
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:24:29 +0800

    #41 podcast-list

---
+++ src/app/components/podcast-list/podcast-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcast-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './podcast-list.component.html',
+  styleUrl: './podcast-list.component.css'
+})
+export class PodcastListComponent {
+
+}
