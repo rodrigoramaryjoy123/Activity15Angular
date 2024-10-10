c5f096d586990d95db8b0deab57ebf4d613d44e8
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 21:50:24 +0800

    #8 car-model-list

---
+++ src/app/components/car-model-list/car-model-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-model-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-model-list.component.html',
+  styleUrl: './car-model-list.component.css'
+})
+export class CarModelListComponent {
+
+}
