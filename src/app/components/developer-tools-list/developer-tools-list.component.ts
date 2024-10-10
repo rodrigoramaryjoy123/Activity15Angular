d6a3eb842e9532814af0c8c61dde438f66bef562
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:27:00 +0800

    #48 developer-tools-list

---
+++ src/app/components/developer-tools-list/developer-tools-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-developer-tools-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './developer-tools-list.component.html',
+  styleUrl: './developer-tools-list.component.css'
+})
+export class DeveloperToolsListComponent {
+
+}
