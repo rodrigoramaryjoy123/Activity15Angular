94fc0d9b708db5722e786841dc52bb3e45484c75
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:20:56 +0800

    #31 computer-hardware-list

---
+++ src/app/components/computer-hardware-list/computer-hardware-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-computer-hardware-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './computer-hardware-list.component.html',
+  styleUrl: './computer-hardware-list.component.css'
+})
+export class ComputerHardwareListComponent {
+
+}
