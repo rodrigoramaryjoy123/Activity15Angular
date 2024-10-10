181d7b4707d90ed30d5dff238c098d5a881dcacd
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:12:08 +0800

    #23 classroom-list

---
+++ src/app/components/classroom-list/classroom-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classroom-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './classroom-list.component.html',
+  styleUrl: './classroom-list.component.css'
+})
+export class ClassroomListComponent {
+
+}
