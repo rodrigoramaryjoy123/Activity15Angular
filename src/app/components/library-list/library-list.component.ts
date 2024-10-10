4b057e888c043937cbdf662c0f31e1f04f42ce67
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:27:46 +0800

    #50 library-list

---
+++ src/app/components/library-list/library-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './library-list.component.html',
+  styleUrl: './library-list.component.css'
+})
+export class LibraryListComponent {
+
+}
