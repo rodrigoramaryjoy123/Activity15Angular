643910267771d6ff58d43787b0fe0f4aded875de
rodrigoramaryjoy123 <rodrigoramaryjoy@gmail.com>
2024-10-10 22:21:19 +0800

    #32 mobile-app-list

---
+++ src/app/components/mobile-app-list/mobile-app-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-app-list.component.html',
+  styleUrl: './mobile-app-list.component.css'
+})
+export class MobileAppListComponent {
+
+}
